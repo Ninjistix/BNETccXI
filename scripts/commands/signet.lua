d6079be8a79661c5f4ee3c44d60f2380ebad6541
Khoa Nguyễn <khoa002@users.noreply.github.com>
2018-03-24 15:17:01 -0700

    Add !signet command with duration based on current rank (#6)
    * Add signet command based on current rank
    
    * Add message to user

---
+++ scripts/commands/signet.lua
@@ -0,0 +1,19 @@
+---------------------------------------------------------------------------------------------------
+-- func: signet
+-- desc: Add signet and shit
+---------------------------------------------------------------------------------------------------
+
+cmdprops = {
+    permission = 0,
+    parameters = ""
+};
+
+function onTrigger(player)
+    local duration = (player:getRank() + getNationRank(player:getNation()) + 3) * 3600;
+    player:delStatusEffect(EFFECT_SIGIL);
+    player:delStatusEffect(EFFECT_SANCTION);
+    player:delStatusEffect(EFFECT_SIGNET);
+    player:addStatusEffect(EFFECT_SIGNET,0,0,duration); -- Grant Signet
+
+    player:PrintToPlayer("Signet granted for " .. tostring(duration / 3600) .. " hours.", 0xF);
+end
