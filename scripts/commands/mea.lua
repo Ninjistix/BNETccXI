965c67691abc1b55c52c01d278d6be4615fa99c7
Khoa Nguyễn <khoa002@gmail.com>
2018-03-24 12:24:42 -0700

    Also adding combat check for !dem, !mea and !holla

--- scripts/commands/mea.lua
+++ scripts/commands/mea.lua
@@ -9,5 +9,10 @@ cmdprops =
 };
 
 function onTrigger(player, x, y, z, zone)
+    if (player:isEngaged() or player:hasEnmity()) then
+        player:PrintToPlayer('This command cannot be used while in combat or under attack! Run away!', 0xF);
+        return;
+    end
+
     player:setPos('95', '35', '340', '245', '117');
 end;
