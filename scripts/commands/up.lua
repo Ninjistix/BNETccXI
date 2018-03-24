5f89013840c9d2d2b3406845ab2e548c0197bf56
Ninji <ninji418@gmail.com>
2018-03-24 08:23:46 -0400

    import up and down from live server.

--- scripts/commands/up.lua
+++ scripts/commands/up.lua
@@ -5,28 +5,27 @@
 
 cmdprops =
 {
-    permission = 1,
+    permission = 0,
     parameters = "is"
 };
 
 function onTrigger(player, number, target)
     local VALUE = 0;
 
-    if (target == nil) then
-        target = player:getName();
-    end
+    -- if (target == nil) then
+        -- target = player:getName();
+    -- end
 
-    local targ = GetPlayerByName( target );
-    if (targ == nil) then
-        player:PrintToPlayer( string.format( "Player named '%s' not found!", target ) );
-        return
-    end
+    local targ = player
+    -- if (targ == nil) then
+        -- player:PrintToPlayer( string.format( "Player named '%s' not found!", target ) );
+        -- return
+    -- end
 
     if (number ~= nil and number > 0) then
         VALUE = targ:getYPos() -number;
     else
         VALUE = targ:getYPos() -1;
     end
-
     targ:setPos(targ:getXPos(), VALUE, targ:getZPos(), targ:getRotPos());
-end
+end;
