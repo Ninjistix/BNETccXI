43970fbcfce4871365e9f1b1ece18a97fe8ac346
Khoa Nguyễn <khoa002@users.noreply.github.com>
2018-03-25 05:55:15 -0700

    Add combat checks to zone and dem/holla/mea commands, also some code formatting (#7)
    * Add command to register all crystal home points for the current player
    
    * Change complete message a bit
    
    * Adding check for if home point name is false
    
    * Add command to add signet based on current rank
    
    * Add combat check for !zone, also a few code formatting changes.
    
    * Also adding combat check for !dem, !mea and !holla

--- scripts/commands/dem.lua
+++ scripts/commands/dem.lua
@@ -9,5 +9,10 @@ cmdprops =
 };
 
 function onTrigger(player, x, y, z, zone)
+    if (player:isEngaged() or player:hasEnmity()) then
+        player:PrintToPlayer('This command cannot be used while in combat or under attack! Run away!', 0xF);
+        return;
+    end
+
     player:setPos('220', '19', '303', '245', '108');
 end;
